1b79778d6f26ed9d7743e731f738425b60287168
erfan <erfan.seidipoor@ut.ac.ir>
2018-04-06 00:02:25 +0430

    third commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,42 @@
 'use strict';
 
-var template = React.createElement(
-  'p',
-  null,
-  ' This is jsx from app.js '
-);
-var appRoot = document.getElementById('app');
+var user = 'Erfan';
+var userAge = 80;
 
+var getAge = function getAge(userAge_) {
+    return userAge_;
+};
+
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'p',
+        null,
+        'This is a test paragraph'
+    ),
+    React.createElement(
+        'div',
+        null,
+        getAge(userAge)
+    ),
+    React.createElement(
+        'div',
+        null,
+        userAge > 19 ? React.createElement(
+            'b',
+            null,
+            'big'
+        ) : 'small'
+    ),
+    React.createElement('div', null),
+    React.createElement(
+        'div',
+        null,
+        ' blue color ',
+        user.toUpperCase()
+    )
+);
+
+var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 
-var template  = <p> This is jsx from app.js </p>
-var appRoot   =   document.getElementById('app')
+var user  = 'Erfan'
+var userAge = 80;
 
+let getAge = (userAge_)=>{
+    return userAge_;
+}
+
+var template  = 
+    <div> 
+        <p>This is a test paragraph</p>
+        <div>{getAge(userAge)}</div>
+        <div>{userAge>19?<b>big</b>:'small'}</div>
+        <div></div>
+        <div> blue color {user.toUpperCase()}</div>
+    </div>
+
+
+var appRoot   =   document.getElementById('app')
 ReactDOM.render (template,appRoot)
