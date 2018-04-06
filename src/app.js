333e1deaeb30045a77d685d74263b83094eff342
erfan <erfan.seidipoor@ut.ac.ir>
2018-04-06 12:46:40 +0430

    sass scss

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 
-import './styles/style_css.css'
+import './styles/style_sass.sass'
 
 let template  = <p> This idfdfwerwer jsx from app.js erfan  </p>
 let appRoot   =   document.getElementById('app')
--- webpack.config.js
+++ webpack.config.js
@@ -40,9 +40,26 @@ const path = require('path');
                           test:/\.css$/,
                           use:[
                               {loader:"style-loader"},
-                              {loader:"css-loader"}
+                              {loader:"css-loader"},
                           ]
+                      },
+                      {
+                        test:/\.scss$/,
+                        use:[
+                            {loader:"style-loader"},
+                            {loader:"css-loader"},
+                            {loader:"sass-loader"},
+                        ]
+                      },
+                      {
+                        test:/\.sass$/,
+                        use:[
+                            {loader:"style-loader"},
+                            {loader:"css-loader"},
+                            {loader:"sass-loader"},
+                        ]
                       }
+
                     ]
                 }
         }
