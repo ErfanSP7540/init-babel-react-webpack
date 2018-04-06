689b633778339c44bd5aa0b14f896c0b77cce9d6
erfan <erfan.seidipoor@ut.ac.ir>
2018-04-06 13:04:38 +0430

    normalize.css

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 
+import '../node_modules/normalize.css/normalize.css'
 import './styles/style_less.less'
 
-let template  = <p> This idfdfwerwer jsx from app.js erfan  </p>
+let template  = <p> This idfdfwerwer jsx from app.js e rfan  </p>
 let appRoot   =   document.getElementById('app')
 
 ReactDOM.render (template,appRoot) 
